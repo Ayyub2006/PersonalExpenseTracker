8304a7f5fd64db09970430afdc7f52b3e37ae930
Ayyub2006 <ayyubkhan0607@gmail.com>
2026-07-09 16:57:01 +0530

    feat: implement custom UserDetailsService

---
+++ src/main/java/com/ayyub/PersonalExpenseTracker/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.ayyub.PersonalExpenseTracker.service;
+
+import com.ayyub.PersonalExpenseTracker.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ayyub.PersonalExpenseTracker.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+	private final UserRepository userRepository;
+	
+	public CustomUserDetailsService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+    @Override
+	public UserDetails loadUserByUsername(String email)
+	throws UsernameNotFoundException{
+		
+		User user = userRepository.findByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+		
+		return org.springframework.security.core.userdetails.User
+				.withUsername(user.getEmail())
+				.password(user.getPassword())
+				.authorities("USER")
+				.build();
+	}
+}
