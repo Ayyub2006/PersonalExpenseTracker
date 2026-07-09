cb9a8bf5c0725407866f43d3855e350ea8676c2e
Ayyub2006 <ayyubkhan0607@gmail.com>
2026-07-09 16:25:20 +0530

    feat: implement JWT token generation

--- src/main/java/com/ayyub/PersonalExpenseTracker/service/JwtService.java
+++ src/main/java/com/ayyub/PersonalExpenseTracker/service/JwtService.java
@@ -1,12 +1,14 @@
 package com.ayyub.PersonalExpenseTracker.service;
 
 import java.util.Date;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -30,6 +32,34 @@ public class JwtService {
 				.expiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(getSignKey())
 				.compact();
+	}
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts.parser()
+				.verifyWith(getSignKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+	}
+	
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public <T> T extractClaim(String token, Function<Claims, T> resolve) {
+		Claims claims = extractAllClaims(token);
+		return resolve.apply(claims);
+	}
+	
+	public boolean isTokenValid(String token, String email) {
+		String username = extractUsername(token);
+		return username.equals(email) && !isTokenExpired(token);
+	}
+	
+	private  boolean isTokenExpired(String token) {
+		return extractAllClaims(token)
+				.getExpiration()
+				.before(new Date());
 	}
 	
 }
